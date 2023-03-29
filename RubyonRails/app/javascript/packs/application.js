bdf8eeb86444307033976e750f6bc1fab780850e
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2023-03-29 18:34:45 -0500

    added tabs, kinda glitchy

--- RubyonRails/app/javascript/packs/application.js
+++ RubyonRails/app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'controllers'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -30,4 +30,20 @@ function hideInput(***REMOVED*** {
 
 
 
+document.addEventListener("turbolinks:load", function(***REMOVED*** {
+  // reset the active tab when the page is loaded
+  $(".tab-content.active"***REMOVED***.removeClass("active"***REMOVED***;
+  $(".tab.active"***REMOVED***.removeClass("active"***REMOVED***;
+
+  // set the active tab based on the URL anchor
+  var tabAnchor = window.location.hash.substring(1***REMOVED***;
+  $("#" + tabAnchor + "-content"***REMOVED***.addClass("active"***REMOVED***;
+  $("#" + tabAnchor + "-tab"***REMOVED***.addClass("active"***REMOVED***;
+
+  // update the URL anchor when a tab is clicked
+  $(".tab"***REMOVED***.on("click", function(***REMOVED*** {
+    var tabAnchor = $(this***REMOVED***.attr("id"***REMOVED***;
+    window.location.hash = tabAnchor;
+  ***REMOVED******REMOVED***;
+***REMOVED******REMOVED***;
 
