74a99576f4a238d5923d1dbb0d595f3cd4dd5625
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2023-03-30 04:58:24 -0500

    fixed home page

---
+++ RubyonRails/app/javascript/packs/custom_tabs.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(***REMOVED*** {
+    const defaultTab = document.getElementById('users-tab'***REMOVED***;
+    if (!defaultTab.classList.contains('active'***REMOVED******REMOVED*** {
+      defaultTab.classList.add('active'***REMOVED***;
+    ***REMOVED***
+  
+    const defaultTabContent = document.getElementById('users-content'***REMOVED***;
+    if (!defaultTabContent.classList.contains('active'***REMOVED******REMOVED*** {
+      defaultTabContent.classList.add('active'***REMOVED***;
+    ***REMOVED***
+  ***REMOVED******REMOVED***;
+  
