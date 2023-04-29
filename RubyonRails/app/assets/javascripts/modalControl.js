d421cf06fbddc24c164bcb62b87acb18aa91758b
Lonnie Easterling <lbot239@gmail.com>
2023-04-29 17:15:02 -0500

    calibration, home page, and other beautification stuff

---
+++ RubyonRails/app/assets/javascripts/modalControl.js
@@ -0,0 +1,6 @@
+$('#myModal'***REMOVED***.on('hide.bs.modal', function (***REMOVED*** { //Change #myModal with your modal id
+    $('audio'***REMOVED***.each(function(***REMOVED***{
+      this.pause(***REMOVED***; // Stop playing
+      this.currentTime = 0; // Reset time
+    ***REMOVED******REMOVED***; 
+***REMOVED******REMOVED***
