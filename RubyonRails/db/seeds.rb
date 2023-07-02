f693d60a0ae794acaa698a172c4f5383f21e5df8
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2023-07-02 17:44:17 -0500

    added tenants, keys, still working on getting it working, got an error when trying to run, dont pull code yet

--- RubyonRails/db/seeds.rb
+++ RubyonRails/db/seeds.rb
@@ -1,60 +1,102 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup***REMOVED***.
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' ***REMOVED***, { name: 'Lord of the Rings' ***REMOVED***]***REMOVED***
-#   Character.create(name: 'Luke', movie: movies.first***REMOVED***
-
-load(Rails.root.join('db', 'seeds', 'clients_seed.rb'***REMOVED******REMOVED***
 # db/seeds.rb
 
-# Generate Users
-# Create 2 local moderators
-2.times do
-  User.create!(
-    email: Faker::Internet.unique.email,
-    password: 'password123',
-    password_confirmation: 'password123',
-    fname: Faker::Name.first_name,
-    lname: Faker::Name.last_name,
-    role: :local_moderator
-  ***REMOVED***
-end
+keys = Key.create!([
+  { code: 'key1', used: false ***REMOVED***,
+  { code: 'key2', used: false ***REMOVED***,
+]***REMOVED***
 
-# Create 9 regular users
-9.times do
-  User.create!(
-    email: Faker::Internet.unique.email,
-    password: 'password123',
-    password_confirmation: 'password123',
-    fname: Faker::Name.first_name,
-    lname: Faker::Name.last_name,
-    role: :regular_user
-  ***REMOVED***
-end
+puts "keys: #{keys.inspect***REMOVED***"
 
-  
-  users = User.all
+tenants = Tenant.create!([
+  { subdomain: 'tenant1' ***REMOVED***,
+  { subdomain: 'tenant2' ***REMOVED***,
+]***REMOVED***
+puts "tenant: #{tenants.inspect***REMOVED***"
+
+
+
+users = User.create!([
+  { email: "globalmod@gmail.com", password: "password", fname: "Global", lname:"Mod", role: :global_moderator, tenant: tenants.first,registration_key: keys.first.code, subdomain: 'subdomain1'***REMOVED***,
     
-  clients = Client.all
-  
-  # Generate Tests
-  3.times do
-    Test.create!(
-      user: users.sample,
-      client: clients.sample,
-      client_name: "#{Faker::Name.first_name***REMOVED*** #{Faker::Name.last_name***REMOVED***",
-      ear_advantage: %w[Left Right].sample,
-      ear_advantage_score: rand(1.0..10.0***REMOVED***.round(2***REMOVED***,
-      label: Faker::Lorem.word,
-      left_score: rand(1.0..10.0***REMOVED***.round(2***REMOVED***,
-      notes: Faker::Lorem.sentence,
-      right_score: rand(1.0..10.0***REMOVED***.round(2***REMOVED***,
-      test_type: %w[DWT RDT].sample
-    ***REMOVED***
-  end
-  
-User.create(email: "globalmod@gmail.com", password: "password", fname: "Global", lname:"Mod", role: :global_moderator***REMOVED***
-User.create(email: "localmod@gmail.com", password: "password",fname: "Local", lname:"Mod", role: :local_moderator***REMOVED***
-User.create(email: "user@gmail.com", password: "password",fname: "Regular", lname:"User", role: :regular_user***REMOVED***
+  { email: "localmod@gmail.com", password: "password",fname: "Local", lname:"Mod", role: :local_moderator, tenant: tenants.second,registration_key: keys.second.code, subdomain: 'subdomain2'
+  ***REMOVED***
+]***REMOVED***
+
+puts "Users: #{users.inspect***REMOVED***"
+
+clients = Client.create!([
+  { 
+    first_name: 'Client1',
+    last_name: 'Test',
+    email: 'client1@test.com',
+    date_of_birth: 30.years.ago,
+    address1: '123 Street',
+    country: 'US',
+    state: 'NY',
+    city: 'New York',
+    zip: '10001',
+    phone1: '1234567890',
+    tenant: tenants.first 
+  ***REMOVED***,
+  { 
+    first_name: 'Client2',
+    last_name: 'Test',
+    email: 'client2@test.com',
+    date_of_birth: 35.years.ago,
+    address1: '456 Street',
+    country: 'US',
+    state: 'CA',
+    city: 'Los Angeles',
+    zip: '90001',
+    phone1: '9876543210',
+    tenant: tenants.second
+  ***REMOVED***,
+]***REMOVED***
+
+puts "Clients: #{clients.inspect***REMOVED***"
+
+emergency_contacts = EmergencyContact.create!([
+  { 
+    first_name: 'Contact1',
+    last_name: 'Test',
+    phone_number: '1234567890',
+    address: '123 Street',
+    email: 'contact1@test.com',
+    city: 'New York',
+    state: 'NY',
+    client: clients.first,
+    tenant: tenants.first
+  ***REMOVED***,
+  { 
+    first_name: 'Contact2',
+    last_name: 'Test',
+    phone_number: '9876543210',
+    address: '456 Street',
+    email: 'contact2@test.com',
+    city: 'Los Angeles',
+    state: 'CA',
+    client: clients.second,
+    tenant: tenants.second
+  ***REMOVED***,
+]***REMOVED***
+
+puts "Emergency Contacts: #{emergency_contacts.inspect***REMOVED***"
+
+tests = Test.create!([
+  {
+    client_name: 'Client1 Test',
+    test_type: 'Type1',
+    client: clients.first,
+    tenant: tenants.first,
+    user: users.first
+  ***REMOVED***,
+  {
+    client_name: 'Client2 Test',
+    test_type: 'Type2',
+    client: clients.second,
+    tenant: tenants.second,
+    user: users.second
+  ***REMOVED***,
+]***REMOVED***
+
+puts "Tests: #{tests.inspect***REMOVED***"
