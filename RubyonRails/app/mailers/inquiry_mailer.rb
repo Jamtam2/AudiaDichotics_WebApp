8249fc3bb4fd916f79f8d066caf764cb03f51b8f
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2023-11-20 19:47:12 -0600

    currently WIP for automation inquiry button for global dataset, not working right now

---
+++ RubyonRails/app/mailers/inquiry_mailer.rb
@@ -0,0 +1,14 @@
+class InquiryMailer < ApplicationMailer
+    default from: 'dichoticdataresearch@gmail.com'
+  
+    def inquiry_email(inquiry***REMOVED***
+      @inquiry = inquiry
+      mail(to: 'dichoticdataresearch@gmail.com', subject: 'New Dataset Inquiry'***REMOVED***
+    end
+  
+    def confirmation_email(inquiry***REMOVED***
+      @inquiry = inquiry
+      mail(to: @inquiry.email, subject: 'We have received your inquiry'***REMOVED***
+    end
+  end
+  
