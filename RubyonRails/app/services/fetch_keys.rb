faa5a4a206221d516f5b7cd8558958f11c7540ec
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2023-12-03 11:22:57 -0600

    added email column, and updated api

--- RubyonRails/app/services/fetch_keys.rb
+++ RubyonRails/app/services/fetch_keys.rb
@@ -8,18 +8,33 @@ class FetchKeys
   base_uri 'api.dichoticsinc.com/api'
 
   def self.call
-    response = get("/license", query: { apiKey: '[CHANGE API KEY WHEN RUNNING]' ***REMOVED******REMOVED***
-    if response.success?
-      response.parsed_response.each do |data|
-        create_key(data***REMOVED*** if data['expiration'] && Time.parse(data['expiration']***REMOVED*** > Time.now
-        
-      end
-    else
-      Rails.logger.error("FetchKeys: Error fetching data - #{response.message***REMOVED***"***REMOVED***
-    end
+    fetch_keys
+    fetch_customers
   end
 
   private
+
+  def self.fetch_keys
+    response = get("/license", query: { apiKey: 'ears_lobo_audia_dichotic_capstone_winter23' ***REMOVED******REMOVED***
+    if response.success?
+      response.parsed_response.each do |data|
+        create_key(data***REMOVED*** if data['expiration'] && Time.parse(data['expiration']***REMOVED*** > Time.now
+      end
+    else
+      Rails.logger.error("FetchKeys: Error fetching license data - #{response.message***REMOVED***"***REMOVED***
+    end
+  end
+
+  def self.fetch_customers
+    response = get("/customer", query: { apiKey: 'ears_lobo_audia_dichotic_capstone_winter23' ***REMOVED******REMOVED***
+    if response.success?
+      response.parsed_response.each do |data|
+        update_key_email(data***REMOVED***
+      end
+    else
+      Rails.logger.error("FetchKeys: Error fetching customer data - #{response.message***REMOVED***"***REMOVED***
+    end
+  end
 
   def self.create_key(data***REMOVED***
     key = Key.find_or_initialize_by(license_id: data['licenseID']***REMOVED***
@@ -32,6 +47,10 @@ class FetchKeys
       subscription_id: data['subscriptiontID']
     ***REMOVED***
     key.save if key.new_record? || key.changed?
-    puts "This license expires: #{data['expiration']***REMOVED***"
-    end
   end
+
+  def self.update_key_email(customer_data***REMOVED***
+    key = Key.find_by(customer_id: customer_data['customerID']***REMOVED***
+    key.update(email: customer_data['email']***REMOVED*** if key
+  end
+end
