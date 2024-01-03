40fce6a085bf61c5dc118d929b886af24375206a
Matt Hosier <mrhosier42@gmail.com>
2024-01-03 00:03:21 -0600

    - Fixed several bugs, including changing when a new local mod is created, it does not change the key to 'used' until it is ensured that it is saved in the database. - Fixed some bugs that prevented user from signing in even with a valid key, this had to do with the way we added an additional required parameter when data is submitted to the database (para: :moderator_code). This is not completely fixed, as reguar and global mod accounts will still need to bypass this or make sure it matches their local mod. - Commented out Unique Code column in the Users/Moderators table. -

--- RubyonRails/app/controllers/registrations_controller.rb
+++ RubyonRails/app/controllers/registrations_controller.rb
@@ -1,44 +1,41 @@
 class RegistrationsController < Devise::RegistrationsController
 
-def create
-    # TODO: This was crashing app before, commented out.
-    # In registration form, user selects regular or local mod
-    # if user.regular
-    # # do something here
-    # end
-    #
-    # if user.local_moderator
-    # end
+  def create
+      # TODO: This was crashing app before, commented out.
+      # In registration form, user selects regular or local mod
+      # if user.regular
+      # # do something here
+      # end
+      #
+      # if user.local_moderator
+      # end
 
+      tenant = Tenant.create!
+      user = nil
+      ActsAsTenant.with_tenant(tenant***REMOVED*** do
+        user = User.new(sign_up_params***REMOVED*** # Initialize new user
+        user.role = :local_moderator    # Set the role
+        key = Key.find_by(activation_code: user.registration_key***REMOVED***
 
-    tenant = Tenant.create!  # Ensure this line is creating a tenant
-    user = nil
-    ActsAsTenant.with_tenant(tenant***REMOVED*** do
-      user = User.create!(sign_up_params***REMOVED***
-    end
-
-    # Check the registration key validity
-    key = Key.find_by(activation_code: user.registration_key***REMOVED***
-
-    if valid_registration_key?(key***REMOVED***
-      key.update(used: true***REMOVED***
-      key.update(email: user.email***REMOVED***
-      user.verification_key = key.activation_code
-      user.role = :local_moderator
-      # Sign in the user and redirect to the homepage if successful login
-      if user.save
-        secret_key = ROTP::Base32.random_base32 # Generate a random secret key
-        user.user_mfa_sessions.create!(secret_key: secret_key, activated: false***REMOVED***
-        puts "Key has been paired with user: #{key.inspect***REMOVED***"
-        puts "User: #{user.inspect***REMOVED***"
-        sign_in(:user, user***REMOVED***
-        redirect_to root_path, notice: 'User was successfully created set up 2FA auth.'
+        if valid_registration_key?(key***REMOVED***
+          # Save the user, which will trigger before_create callback
+          if user.save
+            key.update(used: true, email: user.email***REMOVED***
+            user.verification_key = key.activation_code
+            secret_key = ROTP::Base32.random_base32
+            user.user_mfa_sessions.create!(secret_key: secret_key, activated: false***REMOVED***
+            sign_in(:user, user***REMOVED***
+            redirect_to root_path, notice: 'User was successfully created set up 2FA auth.'
+          else
+            flash[:alert] = 'An internal error occured.'
+            Rails.logger.info("DEBUG: Failed to save user: #{user.errors.full_messages***REMOVED***"***REMOVED***
+            redirect_to new_user_registration_path and return
+          end
+        else
+          flash[:alert] = 'Invalid registration key.'
+          redirect_to new_user_registration_path and return
+        end
       end
-
-    else
-      flash[:alert] = 'Invalid registration key.'
-      redirect_to new_user_registration_path and return
-    end
   end
 
   private
