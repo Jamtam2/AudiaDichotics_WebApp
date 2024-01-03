4ac7b88bc994af66dac803b2b11bf1b779998d2c
Matt Hosier <mrhosier42@gmail.com>
2024-01-02 23:17:30 -0600

    - Fixed bug where clipboard was not copying contents. Included script to load clipboard into every web page that uses it. - In previous commit, organized code styling and javascript code into separate files, fixed file referencing here

--- RubyonRails/app/assets/javascripts/clipboard.js
+++ RubyonRails/app/assets/javascripts/clipboard.js
@@ -1,18 +1,20 @@
-document.addEventListener('DOMContentLoaded', (***REMOVED*** => {
+document.addEventListener('turbolinks:load', (***REMOVED*** => {
     const copyButton = document.getElementById('copy-button'***REMOVED***;
     const copyTarget = document.getElementById('copy-target'***REMOVED***;
 
-    copyButton.addEventListener('click', (***REMOVED*** => {
-        // Copy text to clipboard
-        navigator.clipboard.writeText(copyTarget.placeholder***REMOVED***.then((***REMOVED*** => {
-            // Provide feedback to user
-            const originalText = copyButton.textContent;
-            copyButton.textContent = 'Copied!';
-            setTimeout((***REMOVED*** => {
-                copyButton.textContent = originalText;
-            ***REMOVED***, 2000***REMOVED***;
-        ***REMOVED******REMOVED***.catch(err => {
-            console.error('Error copying text: ', err***REMOVED***;
+    if (copyButton && copyTarget***REMOVED*** {
+        copyButton.addEventListener('click', (***REMOVED*** => {
+            // Copy text to clipboard
+            navigator.clipboard.writeText(copyTarget.value***REMOVED***.then((***REMOVED*** => {
+                // Provide feedback to user
+                const originalText = copyButton.textContent;
+                copyButton.textContent = 'Copied!';
+                setTimeout((***REMOVED*** => {
+                    copyButton.textContent = originalText;
+                ***REMOVED***, 2000***REMOVED***;
+            ***REMOVED******REMOVED***.catch(err => {
+                console.error('Error copying text: ', err***REMOVED***;
+            ***REMOVED******REMOVED***;
         ***REMOVED******REMOVED***;
-    ***REMOVED******REMOVED***;
+    ***REMOVED***
 ***REMOVED******REMOVED***;
