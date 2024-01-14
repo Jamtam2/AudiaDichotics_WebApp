50f3b015b206f3ad38df4a04b4f3b4774d78392f
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2024-01-14 06:08:41 -0600

    billing dashboard setting if user is a local moderator

---
+++ RubyonRails/app/controllers/billing_dashboard_controller.rb
@@ -0,0 +1,37 @@
+# app/controllers/billing_dashboard_controller.rb
+
+class BillingDashboardController < ApplicationController
+    before_action :set_local_moderator
+  
+    def index
+      @current_month_tests = current_tests
+      @previous_tests = previous_tests
+    end
+  
+    private
+  
+    def set_local_moderator
+      # Ensure the user is a local moderator
+      @user = current_user
+      redirect_to(root_url***REMOVED*** unless @user.local_moderator?
+    end
+  
+    def current_tests
+      current_month = Time.now.beginning_of_month..Time.now.end_of_month
+      fetch_tests(current_month***REMOVED***
+    end
+  
+    def previous_tests
+      previous_month = 1.month.ago.beginning_of_month..1.month.ago.end_of_month
+      fetch_tests(previous_month***REMOVED***
+    end
+  
+    def fetch_tests(range***REMOVED***
+      {
+        rddt_tests: @user.rddt_tests.where(created_at: range***REMOVED***,
+        dnw_tests: @user.dnw_tests.where(created_at: range***REMOVED***,
+        dwt_tests: @user.dwt_tests.where(created_at: range***REMOVED***
+      ***REMOVED***
+    end
+  end
+  
