b0f63ca9eba05ab51d94087f7a9755c36a98e438
Hunter <hjckson6@memphis.edu>
2024-02-16 00:04:00 -0600

    Location moderator can now be created and added to an enterprise account

--- RubyonRails/app/assets/javascripts/accountSignupButtons.js
+++ RubyonRails/app/assets/javascripts/accountSignupButtons.js
@@ -1,9 +1,13 @@
 document.addEventListener('turbolinks:load', function (***REMOVED*** {
     let localModeratorRadioButton = document.getElementById('localModerator'***REMOVED***;
     let regularUserRadioButton = document.getElementById('regularUser'***REMOVED***;
+    let locationModeratorRadioButton = document.getElementById('locationModerator'***REMOVED***;
+
 
     localModeratorRadioButton.addEventListener('change', toggleFields***REMOVED***;
     regularUserRadioButton.addEventListener('change', toggleFields***REMOVED***;
+    locationModeratorRadioButton.addEventListener('change', toggleFields***REMOVED***;
+
 
     toggleFields(***REMOVED***; // Initialize the form with the correct fields visible
 ***REMOVED******REMOVED***;
@@ -11,10 +15,12 @@ document.addEventListener('turbolinks:load', function (***REMOVED*** {
 function toggleFields(***REMOVED*** {
     let localModerator = document.getElementById('localModerator'***REMOVED***.checked;
     let regularUser = document.getElementById('regularUser'***REMOVED***.checked;
+    let locationModerator = document.getElementById('locationModerator'***REMOVED***.checked;
+
 
     let registrationKeyField = document.getElementById('registrationKeyField'***REMOVED***;
     let signUpCodeField = document.getElementById('signUpCodeField'***REMOVED***;
 
     registrationKeyField.style.display = localModerator || regularUser ? 'block' : 'none';
-    signUpCodeField.style.display = regularUser ? 'block' : 'none';
+    signUpCodeField.style.display = regularUser || locationModerator ? 'block' : 'none';
 ***REMOVED***
