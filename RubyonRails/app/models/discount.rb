9ab48aff79c4badd46bc2d3171f2bf768be2dca6
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2024-02-18 21:08:46 -0600

    changed stripe api keys to env vars

--- RubyonRails/app/models/discount.rb
+++ RubyonRails/app/models/discount.rb
@@ -18,6 +18,7 @@ class Discount < ApplicationRecord
     validates :redemption_quantity, numericality: { only_integer: true, greater_than: 0 ***REMOVED***
     validates :days_until_expiration, numericality: { only_integer: true, greater_than: 0 ***REMOVED***, on: :create
     before_save :set_expiration_date, if: -> { days_until_expiration.present? ***REMOVED***
+    validates :expiration_date, presence: true
 
     private
 
