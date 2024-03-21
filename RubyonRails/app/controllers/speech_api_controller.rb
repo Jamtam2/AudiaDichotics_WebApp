b5d3db420ca9437c03954a6ded9bb5103a9f3d03
Matt McMeans <mrmcmans@memphis.edu>
2024-03-21 13:20:48 -0500

    current recorder progress

--- RubyonRails/app/controllers/speech_api_controller.rb
+++ RubyonRails/app/controllers/speech_api_controller.rb
@@ -5,7 +5,11 @@ class TranscriptionsController < ApplicationController
   def create
     client = AssemblyAI::Client.new(api_key: ENV['8323e4c46ab24be5822b111c7fd30635'])
     audio_data = params[:audio_data] # This should contain the audio data captured from the microphone
-    
+
+    # Access the recorder.js file and capture audio data
+    recorder_js_path = Rails.root.join('app', 'assets', 'javascripts', 'recorder.js')
+    audio_data = `node #{recorder_js_path}`
+
     # Submit the audio data for transcription
     response = client.transcribe(audio_data)
     
