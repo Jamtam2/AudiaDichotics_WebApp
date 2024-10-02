9cff02e813480bc0cafa8afd36d4b751a8eb4705
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2024-10-02 16:26:56 -0500

    added time ranged voice activation; buttons will be filled out only if the time matches with the audio file

--- RubyonRails/app/javascript/packs/transcription.js
+++ RubyonRails/app/javascript/packs/transcription.js
@@ -38,6 +38,7 @@ const dwtList1 = {
 
 const dwtList2 = {
     'form': 'Farm',
+    'ben': 'Den',
     'hoarse': 'Horse',
     'butt': 'But',
     'cag': 'Tag',
@@ -101,6 +102,131 @@ const lookupTable = {
 };
 
 // import { SoxRecording } from './sox.js'; // Import SoxRecording
+  // Access the words array passed from the ERB template
+let words; 
+    if (listType === "dwt_list1"){
+     words = [
+        ["Fun", "Pig"],
+        ["Vine", "Sheep"],
+        ["Cake", "Buy"],
+        ["Round", "Storm"],
+        ["Say", "Tan"],
+        ["Yes", "Fast"],
+        ["Pen", "Most"],
+        ["Bet", "One"],
+        ["Green", "Booth"],
+        ["Store", "Glass"],
+        ["Soap", "Teeth"],
+        ["Bring", "Share"],
+        ["Rug", "Book"],
+        ["Want", "Soft"],
+        ["Tow", "Kite"],
+        ["Why", "Quack"],
+        ["Comb", "For"],
+        ["That", "Love"],
+        ["Bug", "Wet"],
+        ["Quiz", "Juice"],
+        ["Camp", "Light"],
+        ["Hand", "Goose"],
+        ["Lip", "Dad"],
+        ["Fall", "Room"],
+        ["Old", "Chef"]
+        ]
+  }
+    else if (listType === "dwt_list2"){
+         words = [
+            ["Bell", "Farm"],
+            ["Same", "Cast"],
+            ["Child", "Bowl"],
+            ["How", "Plug"],
+            ["Lick", "Top"],
+            ["Watch", "Horse"],
+            ["Ham", "Day"],
+            ["When", "Truck"],
+            ["Drink", "Rhyme"],
+            ["Sleep", "Yard"],
+            ["Greek", "Stop"],
+            ["Den", "Red"],
+            ["Song", "Math"],
+            ["Dog", "More"],
+            ["Spoon", "North"],
+            ["But", "Chip"],
+            ["Year", "Nice"],
+            ["Cloud", "Make"],
+            ["Tag", "Heart"],
+            ["Pot", "Ball"],
+            ["Door", "Set"],
+            ["Cup", "Pat"],
+            ["Close", "Shine"],
+            ["Ring", "Test"],
+            ["Rough", "Corn"]
+            ]
+
+    }
+    else if (listType === "dwt_list3"){
+         words = [
+            ["Post", "Rim"],
+            ["Two", "Look"],
+            ["Cook", "Bite"],
+            ["Car", "Lamp"],
+            ["Skip", "Kiss"],
+            ["Dream", "Floor"],
+            ["Phone", "Damp"],
+            ["Sit", "Gold"],
+            ["Bear", "Sun"],
+            ["Knife", "Bath"],
+            ["Leg", "Milk"],
+            ["Young", "Brush"],
+            ["See", "Law"],
+            ["Cat", "Hug"],
+            ["Grass", "Square"],
+            ["Weird", "Globe"],
+            ["Bird", "Fork"],
+            ["Bus", "Swing"],
+            ["Snow", "Race"],
+            ["Moon", "Wall"],
+            ["Heel", "Five"],
+            ["Mud", "Sheet"],
+            ["Mat", "Nail"],
+            ["Gum", "Pie"],
+            ["Pill", "Key"]
+            ]
+
+    }
+    else if (listType === "dwt_list4"){
+         words = [
+            ["Bar", "Head"],
+            ["Box", "Shelf"],
+            ["Will", "Pan"],
+            ["Work", "Dive"],
+            ["Knit", "Where"],
+            ["Down", "Vase"],
+            ["Food", "Judge"],
+            ["Gas", "Sand"],
+            ["Van", "Game"],
+            ["Joke", "Need"],
+            ["New", "Late"],
+            ["Plate", "Rope"],
+            ["Jar", "Fish"],
+            ["First", "Black"],
+            ["Voice", "Rose"],
+            ["Bed", "Catch"],
+            ["Couch", "Best"],
+            ["Type", "Big"],
+            ["Rain", "Mouse"],
+            ["Cow", "Her"],
+            ["Neat", "Rock"],
+            ["Sing", "Plain"],
+            ["Learn", "Dark"],
+            ["Yell", "White"],
+            ["Help", "Knee"]
+            ]
+    }
+  console.log('Words array:', words);
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
 
     // Create a dictionary of labels and checkboxes
     let labelCheckboxMap = {};
@@ -114,9 +240,20 @@ const lookupTable = {
         }
     });
     console.log('created the label checkbox map: ', labelCheckboxMap)
-document.addEventListener('DOMContentLoaded', function () {
-
     
+      // Create wordTimeWindows
+  let wordTimeWindows = words.map(function (pair, index) {
+    let startTime = index * 5 +5; // Each word pair starts every 5 seconds
+    let endTime = startTime + 15; // Each word pair is acceptable for 15 seconds
+    return {
+      words: pair,
+      startTime: startTime,
+      endTime: endTime,
+    };
+  });
+  console.log('wordTimeWindows:', wordTimeWindows);
+
+
     // Start the Node.js script when the page loads
     fetch('/start_speech_script')  
       .then(response => response.text())
@@ -335,9 +472,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // The checkAnswer function remains the same
     function checkAnswer(transcribedText) {
+        let currentTime = audioPlayer.currentTime
         console.log('Checking answer...');
         console.log('Current audio file time: ', audioPlayer.currentTime)
         console.log('On file: ', listType)
+
+        let validWords = [];
+
+        wordTimeWindows.forEach(function (item) {
+          if (
+            currentTime >= item.startTime &&
+            currentTime <= item.endTime
+          ) {
+            validWords = validWords.concat(item.words);
+          }
+        });
+        console.log('Valid words for current time: ', validWords);
+
         // let wordList = transcribedText.replace(/[^\w\s]|_/g, '').trim().split(/\s+/);
         let wordList = transcribedText.replace(/\./, '').trim().split(/\s+/); // Clean up the transcript
         console.log('Mapping vocab...');
@@ -357,24 +508,27 @@ document.addEventListener('DOMContentLoaded', function () {
         let allLabels = document.querySelectorAll('label');
 
     // Loop through each word in the wordList and check if it exists in the dictionary
+    // Only check checkboxes for words that are in validWords
     wordList.forEach((word) => {
         let key = word.toLowerCase();
-        if (labelCheckboxMap[key]) {
+        if (validWords.map((w) => w.toLowerCase()).includes(key)) {
+          if (labelCheckboxMap[key]) {
             let { checkbox, label } = labelCheckboxMap[key];
             if (!checkbox.checked) {
-                checkbox.checked = true;
-                checkbox.dispatchEvent(
-                    new Event('change', {
-                        bubbles: true,
-                        cancelable: true,
-                    })
-                );
-                label.classList.add('active'); // Provide visual feedback
+              checkbox.checked = true;
+              checkbox.dispatchEvent(
+                new Event('change', {
+                  bubbles: true,
+                  cancelable: true,
+                })
+              );
+              label.classList.add('active'); // Provide visual feedback
             }
+          }
         }
-    });
-}
-
+      });
+    }
+  
         // Stop the Node.js script when the user leaves the page
         window.addEventListener('beforeunload', function() {
             navigator.sendBeacon('/stop_speech_script');
