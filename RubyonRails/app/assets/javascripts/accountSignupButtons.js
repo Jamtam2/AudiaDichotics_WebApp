f6a038abfea5383d7c20686adacae9742dbf5c5f
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2024-10-10 15:24:06 -0500

    fixed tabs.js and bootcamp removal

--- RubyonRails/app/assets/javascripts/accountSignupButtons.js
+++ RubyonRails/app/assets/javascripts/accountSignupButtons.js
@@ -11,27 +11,20 @@ document.addEventListener('turbolinks:load', function () {
         regularUserRadioButton.addEventListener('change', toggleFields);
     }
 
-    if (bootcampUserRadioButton) { // New block
-        bootcampUserRadioButton.addEventListener('change', toggleFields);
-    }
-
     toggleFields(); // Initialize the form with the correct fields visible
 });
 
 function toggleFields() {
     let localModeratorRadioButton = document.getElementById('localModerator');
     let regularUserRadioButton = document.getElementById('regularUser');
-    let bootcampUserRadioButton = document.getElementById('bootcampUser'); // New line
 
     // Ensure all radio buttons exist before accessing their checked state
-    if (localModeratorRadioButton && regularUserRadioButton && bootcampUserRadioButton) {
+    if (localModeratorRadioButton && regularUserRadioButton) {
         let localModerator = localModeratorRadioButton.checked;
         let regularUser = regularUserRadioButton.checked;
-        let bootcampUser = bootcampUserRadioButton.checked; // New line
 
         let registrationKeyField = document.getElementById('registrationKeyField');
         let signUpCodeField = document.getElementById('signUpCodeField');
-        let bootcampCodeField = document.getElementById('bootcampCodeField'); // New line
 
         if (registrationKeyField) {
             registrationKeyField.style.display = localModerator ? 'block' : 'none';
@@ -41,8 +34,5 @@ function toggleFields() {
             signUpCodeField.style.display = regularUser ? 'block' : 'none';
         }
 
-        if (bootcampCodeField) { // New block
-            bootcampCodeField.style.display = bootcampUser ? 'block' : 'none';
-        }
     }
 }
