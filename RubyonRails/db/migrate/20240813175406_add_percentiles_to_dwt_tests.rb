ca2e72621bf2d8fc2825076ee43c64eb355e21a3
Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
2024-10-17 11:40:01 -0500

    Delete RubyonRails/db/migrate/20240813175406_add_percentiles_to_dwt_tests.rb

--- RubyonRails/db/migrate/20240813175406_add_percentiles_to_dwt_tests.rb
+++
@@ -1,7 +0,0 @@
-class AddPercentilesToDwtTests < ActiveRecord::Migration[6.1]
-  def change
-    add_column :dwt_tests, :advantage_percentile, :string
-    add_column :dwt_tests, :left_percentile, :string
-    add_column :dwt_tests, :right_percentile, :string
-  end
-end
