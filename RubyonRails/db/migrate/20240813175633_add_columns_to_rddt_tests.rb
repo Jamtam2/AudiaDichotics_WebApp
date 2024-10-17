f223cf7cf07026a8855c5cb082ebcd51c4023706
Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
2024-10-17 11:41:36 -0500

    Delete RubyonRails/db/migrate/20240813175633_add_columns_to_rddt_tests.rb

--- RubyonRails/db/migrate/20240813175633_add_columns_to_rddt_tests.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToRddtTests < ActiveRecord::Migration[6.1]
-  def change
-    add_column :rddt_tests, :advantage_percentile, :string
-    add_column :rddt_tests, :ear_advantage_score1, :float
-    add_column :rddt_tests, :left_percentile, :string
-    add_column :rddt_tests, :ear_advantage_score3, :float
-
-  end
-end
