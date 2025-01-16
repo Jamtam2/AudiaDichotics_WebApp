7f536c25af926584ac43566ea954e0353ef99db1
Bruhamut <bbmaster358@gmail.com>
2025-01-16 15:52:31 -0600

    Progress on ToS validation trigger based on javascript function

--- RubyonRails/app/assets/javascripts/terms_of_service.js
+++ RubyonRails/app/assets/javascripts/terms_of_service.js
@@ -1,25 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const termsButton = document.getElementById("tos-button");
-//     const termsText = document.getElementById("tos-model");
-//     const termsAccept = document.getElementById("tos-accept");
-//     const termsCheckbox = document.getElementById("tos-checkbox");
-
-//     termsButton.addEventListener("click", function() { 
-//         termsText.style.display = 'flex'; 
-//     });
-// });
-
 function showText(event) {
     event.preventDefault();  // Prevent form submission
-    const tosPopup = document.getElementById('tos-model');
-    
-    if (tosPopup) {
-        tosPopup.style.display = 'flex'; // Show the ToS popup
-    } else {
-        console.error('ToS popup element not found!');
-    }
+    document.getElementById('tos-model').style.display = 'flex';
 }
 
-function hideText() {
+function tosAccept(event) {
+    event.preventDefault();
     document.getElementById("tos-model").style.display = 'none';
+    document.getElementById("tos-checkbox").checked = true;
+    document.getElementById("tos-accepted").value = true;
 }
